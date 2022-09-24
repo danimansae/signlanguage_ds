ab6045e81dafc0872b1915d4bc7608d469654191
Huh521 <huhgoo99@gmail.com>
2022-09-24 20:57:27 +0900

    09 / 24 - UI 수정 (1)

--- app/src/main/java/com/google/codelab/mlkit/mMainActivity.java
+++ app/src/main/java/com/google/codelab/mlkit/mMainActivity.java
@@ -126,6 +126,8 @@ public class mMainActivity extends AppCompatActivity implements View.OnClickList
 
         ImageView gif = (ImageView) findViewById(R.id.imageView_camera);
         Glide.with(this).load(R.drawable.camera).into(gif);
+        ImageView gif2 = (ImageView) findViewById(R.id.imageView_album);
+        Glide.with(this).load(R.drawable.album).into(gif2);
 
         sttBtn.setOnClickListener(v -> {
             if ( Build.VERSION.SDK_INT >= 23 ){
@@ -150,99 +152,99 @@ public class mMainActivity extends AppCompatActivity implements View.OnClickList
     }
 
 
-//음성인식
-private RecognitionListener listener = new RecognitionListener() {
-    @Override
-    public void onReadyForSpeech(Bundle params) {
-        Toast.makeText(getApplicationContext(), "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onBeginningOfSpeech() {
-    }
-
-    @Override
-    public void onRmsChanged(float rmsdB) {
-
-    }
-
-    @Override
-    public void onBufferReceived(byte[] buffer) {
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-    }
-
-    @Override
-    public void onError(int error) {
-        String message;
-
-        switch (error) {
-            case SpeechRecognizer.ERROR_AUDIO:
-                message = "오디오 에러";
-                break;
-            case SpeechRecognizer.ERROR_CLIENT:
-                message = "클라이언트 에러";
-                break;
-            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
-                message = "퍼미션 없음";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK:
-                message = "네트워크 에러";
-                break;
-            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
-                message = "네트웍 타임아웃";
-                break;
-            case SpeechRecognizer.ERROR_NO_MATCH:
-                message = "찾을 수 없음";
-                break;
-            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
-                message = "RECOGNIZER가 바쁨";
-                break;
-            case SpeechRecognizer.ERROR_SERVER:
-                message = "서버가 이상함";
-                break;
-            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
-                message = "말하는 시간초과";
-                break;
-            default:
-                message = "알 수 없는 오류임";
-                break;
+    //음성인식
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Toast.makeText(getApplicationContext(), "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
         }
 
-        Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onResults(Bundle results) {
-        // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줌
-        ArrayList<String> matches =
-                results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        for(int i = 0; i < matches.size() ; i++){
-            textView.setText(matches.get(i));
-            strs = matches.get(i).toString();
+        @Override
+        public void onBeginningOfSpeech() {
         }
 
-        //음성 인식으로 출력(추정) 값이 나오는 데 보통 첫번째 배열의 값이 정확도 높음
-        String key = "";
-        key = SpeechRecognizer.RESULTS_RECOGNITION;
-        ArrayList<String> mResult = results.getStringArrayList(key);
-        String[] rs = new String[mResult.size()];
-        mResult.toArray(rs);
-        //rs에 인식된 결과 값이 들어가 있다.
-    }
+        @Override
+        public void onRmsChanged(float rmsdB) {
 
-    @Override
-    public void onPartialResults(Bundle partialResults) {
+        }
 
-    }
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+        }
 
-    @Override
-    public void onEvent(int eventType, Bundle params) {
+        @Override
+        public void onEndOfSpeech() {
+        }
 
-    }
-};
+        @Override
+        public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줌
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for(int i = 0; i < matches.size() ; i++){
+                textView.setText(matches.get(i));
+                strs = matches.get(i).toString();
+            }
+
+            //음성 인식으로 출력(추정) 값이 나오는 데 보통 첫번째 배열의 값이 정확도 높음
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = results.getStringArrayList(key);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            //rs에 인식된 결과 값이 들어가 있다.
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+
+        }
+    };
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
